e4015fcc046a6501f578483344d35723dcfb713f
cucuhead <73545921+cucuhead@users.noreply.github.com>
2026-01-31 17:48:44 +0300

    feat: complete Header and HomePage UI with Figma specs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 1. Bunu ekle
+import App from './App.jsx';
+import './styles/global.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* 2. App'i bununla sarmala */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
