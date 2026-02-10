58f1b4397ce0ba48c2f7e97fe1b7d4fd4fb57b73
cucuhead <73545921+cucuhead@users.noreply.github.com>
2026-02-10 23:08:23 +0300

    feat: add professional loader with scroll lock and final README

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import css from "./Loader.module.css";
 
 const Loader = () => {
+ useEffect(() => {
+  const originalBodyOverflow = document.body.style.overflow;
+  const originalHtmlOverflow = document.documentElement.style.overflow;
+
+  // Hem body hem html'i kilitle
+  document.body.style.overflow = "hidden";
+  document.documentElement.style.overflow = "hidden";
+
+  return () => {
+    document.body.style.overflow = originalBodyOverflow || "unset";
+    document.documentElement.style.overflow = originalHtmlOverflow || "unset";
+  };
+}, []);
+
   return (
     <div className={`${css.loaderWrapper} ${css.noScroll}`}>
       <div className={css.movingContainer}>
-        {/* public klasörüne attığın ikona doğrudan erişim */}
-        <img src="/icons8-camper.png" alt="Loading..." className={css.camper}  />
+        <img 
+          src="/icons8-camper.png" 
+          alt="Loading..." 
+          className={css.camper} 
+        />
         
         <div className={css.road}>
           <div className={css.line}></div>
